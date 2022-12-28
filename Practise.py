5f296ae9f4166a12c4958abfbf7ebb3de6ab974d
Mingxuan Zhao <mingxuanzhao@MingxuandeMacBook-Pro.local>
2022-12-28 14:11:36 +1300

    pratice code

---
+++ Practise.py
@@ -0,0 +1,72 @@
+# Data Types
+
+int = 1
+#positive or negetive whole numbers
+
+float = 1.2
+#real number with a floating point
+
+complex = 12j
+#(real part) + (imaginary part)
+
+str = "Good morning"
+#arrays of bytes
+
+list = ["apple", "banana", "cherry"]
+#a ordered collection of data
+
+tuple = ("apple", "banana", "cherry")
+#tuple is also an ordered collection of Python objects
+#but tuple is immutable
+
+bool = True
+#Data type with one of the two built-in values, True or False
+
+set = {"apple", "banana", "cherry"}
+#an unordered collection of data type
+
+dict = {"name" : "John", "age" : 36}
+#an unordered collection of data values
+
+range = [3,6]
+#a sequence of numbers
+
+frozenset = ({"apple", "banana", "cherry"})
+#an immutable Set object
+
+bytes = b"hi"
+#an immuteble byte object
+
+bytearray = bytearray(5)
+#an array of given bytes
+
+memoryview = memoryview(bytes(5))
+# idk hard to understand
+
+NoneType = None
+#none
+
+
+
+
+#Operators
+a = 3 + 2
+b = 3 - 2
+c = 3 * 2
+d = 3 / 2
+e = 3 % 2
+f = 3 ** 2
+g = 3 // 2
+
+h = 5
+h += 5
+h -= 5
+h *= 5
+h /= 5
+
+x == y
+x != y
+x < y
+x > y
+x <= y
+x >= y
